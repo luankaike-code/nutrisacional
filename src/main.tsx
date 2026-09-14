c09a9eae58c576cf0f17ea0760037e33bb6824fa
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-14 15:23:24 -0300

    Implement Wrapper component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Wrapper } from './components/index.ts'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+		<Wrapper>
+			<App />
+		</Wrapper>
   </StrictMode>,
 )
