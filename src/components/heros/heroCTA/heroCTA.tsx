1279cb00849435e916f4daf2c484c6336af67fbb
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-15 15:08:39 -0300

    Create HeroCTA component

---
+++ src/components/heros/heroCTA/heroCTA.tsx
@@ -0,0 +1,28 @@
+import { HeroBase } from "../components";
+import { Button } from "@/components/ui/button";
+
+function HeroCTA() {
+	return (
+		<HeroBase>
+			<div className="flex flex-col gap-4">
+				<div className="flex flex-col gap-2">
+					<h1 className="font-bold text-4xl">
+						Nutrisacional, um lugar para aprender mais sobre você e sua nutrição
+					</h1>
+					<p>
+						Nutrisacional foi criado para conscientizar você sobre como sua nutrição funciona e como você pode cuidar dela para atingir o seu objetivo.
+					</p>
+					<div className="flex flex-col gap-2">
+						<Button>Comece Agora</Button>
+						<Button variant="outline">Leia Mais</Button>
+					</div>
+				</div>
+				<div>
+					<img src="https://placehold.co/600x800"/>
+				</div>
+			</div>
+		</HeroBase>
+	)
+}
+
+export { HeroCTA }
