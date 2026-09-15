21286a4a5611ae3ee428b47c49a49b00240f210e
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-15 14:29:53 -0300

    Create path alias (./src: @)

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
@@ -10,4 +11,9 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
 		tailwindcss(),
   ],
+	resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
