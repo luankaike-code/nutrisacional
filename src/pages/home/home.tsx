20ae22e6f281069c525d46dffe13ab6d3af10ea5
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-18 15:19:41 -0300

    Implement new heroCTA to home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,15 @@
+import { useNavigate } from "react-router";
 import { HeroCTA } from "./components"
+import { Button } from "@/components/ui/button";
 
 function Home() {
+	const navigate = useNavigate();
   return (
     <>
-      <HeroCTA />
+      <HeroCTA src={"https://placehold.co/600x800"}>
+				<Button onClick={() => navigate("/")}>Use nossa calculadora</Button>
+				<Button onClick={() => navigate("/doc")} variant="outline">Leia mais</Button>
+			</HeroCTA>
     </>
   )
 }
