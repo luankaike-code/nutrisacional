e2ddb3dd448a01bfc1443c5aacceb5935fa19e54
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-21 14:43:20 -0300

    Add links to Header

--- src/components/wrapper/components/header/header.tsx
+++ src/components/wrapper/components/header/header.tsx
@@ -1,10 +1,21 @@
 import CompleteIcon from "@/assets/complete-icon.svg"
+import { useNavigate } from "react-router"
+import { LiLink } from "./components";
 
 function Header() {
+	const navigate = useNavigate();
   return (
-		<header className="h-12 p-2">
-			<img src={CompleteIcon} className="h-full" />
-		</header>
+		<>
+			<header className="fixed flex w-full h-16 p-4 pr-8 bg-white justify-between">
+				<img src={CompleteIcon} className="h-full" onClick={() => navigate("/")}/>
+				<ul className="flex">
+					<LiLink href="/">Início</LiLink>
+					<LiLink href="/doc">Documentação</LiLink>
+					<LiLink href="/doc">Calculadora</LiLink>
+				</ul>
+			</header>
+			<div className="block w-full h-16"></div>
+		</>
   )
 }
 
