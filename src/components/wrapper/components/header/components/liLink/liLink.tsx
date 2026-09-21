7ffc01dba6b95892d5110fafb737524347e17c20
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-21 15:22:57 -0300

    Create feedback to show that is current route to LiLink

--- src/components/wrapper/components/header/components/liLink/liLink.tsx
+++ src/components/wrapper/components/header/components/liLink/liLink.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import type { PropsWithChildren } from "react";
-import { useNavigate } from "react-router";
+import { useNavigate, useLocation } from "react-router";
 
 function LiLink({href, children}: {href: string} & PropsWithChildren) {
+	const location = useLocation()
 	const navigate = useNavigate();
+	
 	return (
 		<li>
-			<Button variant="link" onClick={() => navigate(href)} role="link" className="text-base">
+			<Button variant="link" onClick={() => navigate(href)} role="link" className={location.pathname == href? "text-rose-500": ""}>
 				{children}
 			</Button>
 		</li>
