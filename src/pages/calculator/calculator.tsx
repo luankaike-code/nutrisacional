e64a0f56d8c07225ce11ad5afffdd4e425affe99
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-24 18:12:28 -0300

    Implement new components to Calculator page

--- src/pages/calculator/calculator.tsx
+++ src/pages/calculator/calculator.tsx
@@ -1,9 +1,15 @@
+import { useState } from "react"
+import { CalculatorForm, CalculatorResults } from "./components"
+import type { NutritionalValues } from "./components/calculatorForm/calculatorForm"
+
 function Calculator() {
-	return (
-		<>
-			<h1>Calculator</h1>
-		</>
-	)
+	const [nutritionalValues, setNutritionalValues] = useState<NutritionalValues | null>(null)
+  return (
+		<section className="self-center flex flex-col md:flex-row gap-4 my-12">
+			<CalculatorForm className="min-w-xl" onCalculate={setNutritionalValues} />
+			<CalculatorResults  className="min-w-xl dark" calcResults={nutritionalValues} />
+		</section>
+  )
 }
 
 export { Calculator }
