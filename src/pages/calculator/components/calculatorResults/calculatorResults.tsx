e5017ce42defed3b93bc5324b66c5badc9a03352
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-24 19:03:21 -0300

    Create content if have result to CalculatorResults

--- src/pages/calculator/components/calculatorResults/calculatorResults.tsx
+++ src/pages/calculator/components/calculatorResults/calculatorResults.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import type { NutritionalValues } from "../calculatorForm/calculatorForm"
 import type { HTMLAttributes } from "react"
+import { CalculatorResultsField } from "./components"
 import { cn } from "@/lib/utils"
 import Favicon from "@/../public/favicon.svg"
 
@@ -12,7 +13,7 @@ function CalculatorResults({calcResults, className, ...props}:
 				<CardTitle>Resultados</CardTitle>
 				<CardDescription>Valores nutricionais calculados</CardDescription>
 			</CardHeader>
-			<CardContent className="h-full flex flex-col gap-2">
+			<CardContent className="h-full flex flex-col">
 				{ !calcResults &&
 					<>
 						<div className="flex justify-center items-center h-full w-full gap-2">
@@ -21,16 +22,26 @@ function CalculatorResults({calcResults, className, ...props}:
 					</>
 				}
 				{ calcResults &&
-					<>
-						<div className="flex justify-between">
-							<span>Taxa Basal Metabólica (TBM):</span>
-							<span>{calcResults.TBM.toFixed(2)} cal/dia</span>
-						</div>
-						<div className="flex justify-between">
-							<span>Gasto Energético Total (GET):</span>
-							<span>{calcResults.GET.toFixed(2)} cal/dia</span>
-						</div>
-					</>
+					<ul className="flex flex-col gap-8">
+						<li>
+							<h1 className="text-lg font-semibold">Valores Nutricionais</h1>
+							<ul className="flex flex-col gap-2 p-2">
+								<CalculatorResultsField label="TBM" value={calcResults.TBM} unit="cal/dia" />
+								<CalculatorResultsField label="GET" value={calcResults.GET} unit="cal/dia" />
+								<CalculatorResultsField label="Proteínas por quilo" value={calcResults.gramsProteinToWeightRatio} unit="g/kg" />
+							<CalculatorResultsField label="Carboidratos por quilo" value={calcResults.gramsFatToWeightRatio} unit="g/kg" />
+							</ul>
+						</li>
+
+						<li>
+							<h1 className="text-lg font-semibold">Sugestões de Nutrição</h1>
+							<ul className="flex flex-col gap-2 p-2">
+								<CalculatorResultsField label="Proteínas" value={calcResults.gramsProtein} unit="g/dia" />
+								<CalculatorResultsField label="Carboidratos" value={calcResults.gramsCarbs} unit="g/dia" />
+								<CalculatorResultsField label="Gorduras" value={calcResults.gramsFat} unit="g/dia" />
+							</ul>
+						</li>
+					</ul>
 				}
 			</CardContent>
 		</Card>
