ac4a5b4319a47c40ece5916efdd439fdf9cbea15
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-24 20:42:06 -0300

    Refactore to use useState in result on Calculator

--- src/pages/calculator/components/calculatorResults/calculatorResults.tsx
+++ src/pages/calculator/components/calculatorResults/calculatorResults.tsx
@@ -1,12 +1,18 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import type { NutritionalValues } from "../calculatorForm/calculatorForm"
-import type { HTMLAttributes } from "react"
+import { useEffect, useState, type HTMLAttributes } from "react"
 import { CalculatorResultsField } from "./components"
 import { cn } from "@/lib/utils"
 import Favicon from "@/../public/favicon.svg"
 
 function CalculatorResults({calcResults, className, ...props}: 
 	{calcResults?: NutritionalValues | null} & HTMLAttributes<HTMLDivElement>){
+	const [results, setResults] = useState<NutritionalValues | null>(calcResults?? null)
+
+	useEffect(() => {
+		setResults(calcResults?? null)
+	}, [calcResults])
+	
 	return (
 		<Card className={cn("w-full", className)} {...props}>
 			<CardHeader>
@@ -14,31 +20,31 @@ function CalculatorResults({calcResults, className, ...props}:
 				<CardDescription>Valores nutricionais calculados</CardDescription>
 			</CardHeader>
 			<CardContent className="h-full flex flex-col">
-				{ !calcResults &&
+				{ !results &&
 					<>
 						<div className="flex justify-center items-center h-full w-full gap-2">
 							<img src={Favicon} className="animate-bounce w-1/3" />
 						</div>
 					</>
 				}
-				{ calcResults &&
+				{ results &&
 					<ul className="flex flex-col gap-8">
 						<li>
 							<h1 className="text-lg font-semibold">Valores Nutricionais</h1>
 							<ul className="flex flex-col gap-2 p-2">
-								<CalculatorResultsField label="TBM" value={calcResults.TBM} unit="cal/dia" />
-								<CalculatorResultsField label="GET" value={calcResults.GET} unit="cal/dia" />
-								<CalculatorResultsField label="Proteínas por quilo" value={calcResults.gramsProteinToWeightRatio} unit="g/kg" />
-							<CalculatorResultsField label="Carboidratos por quilo" value={calcResults.gramsFatToWeightRatio} unit="g/kg" />
+								<CalculatorResultsField label="TBM" value={results.TBM} unit="cal/dia" />
+								<CalculatorResultsField label="GET" value={results.GET} unit="cal/dia" />
+								<CalculatorResultsField label="Proteínas por quilo" value={results.gramsProteinToWeightRatio} unit="g/kg" />
+							<CalculatorResultsField label="Carboidratos por quilo" value={results.gramsFatToWeightRatio} unit="g/kg" />
 							</ul>
 						</li>
 
 						<li>
 							<h1 className="text-lg font-semibold">Sugestões de Nutrição</h1>
 							<ul className="flex flex-col gap-2 p-2">
-								<CalculatorResultsField label="Proteínas" value={calcResults.gramsProtein} unit="g/dia" />
-								<CalculatorResultsField label="Carboidratos" value={calcResults.gramsCarbs} unit="g/dia" />
-								<CalculatorResultsField label="Gorduras" value={calcResults.gramsFat} unit="g/dia" />
+								<CalculatorResultsField label="Proteínas" value={results.gramsProtein} unit="g/dia" />
+								<CalculatorResultsField label="Carboidratos" value={results.gramsCarbs} unit="g/dia" />
+								<CalculatorResultsField label="Gorduras" value={results.gramsFat} unit="g/dia" />
 							</ul>
 						</li>
 					</ul>
