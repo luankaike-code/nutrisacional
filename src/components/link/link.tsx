9c46edb32c324dabd0e7c413598ae7cf8ff809cb
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-24 22:40:32 -0300

    Now links can also open external web pages

--- src/components/link/link.tsx
+++ src/components/link/link.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import type { PropsWithChildren } from "react";
+import { useCallback, type PropsWithChildren } from "react";
 import { useNavigate, useLocation } from "react-router";
 
 function Link({href, children}: {href: string} & PropsWithChildren) {
 	const location = useLocation()
 	const navigate = useNavigate();
+
+	const handleClick = useCallback(() => {
+		if (href.startsWith("http")) {
+			window.open(href, "_blank");
+			return;
+		}
+		navigate(href);
+	}, [href, navigate])
 	
 	return (
-		<Button variant="link" onClick={() => navigate(href)} role="link" className={
+		<Button variant="link" onClick={handleClick} role="link" className={
 			cn("hover:text-rose-500", location.pathname === href ? "text-rose-500" : "")
 		}>
 			{children}
