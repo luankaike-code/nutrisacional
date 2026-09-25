82a0c64768c0a2d6a34e06aea869602589075706
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-24 21:12:33 -0300

    Implement mockup image

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,12 +1,13 @@
 import { useNavigate } from "react-router";
 import { HeroCTA } from "./components"
 import { Button } from "@/components/ui/button";
+import Mockup from "@/assets/mockup.png"
 
 function Home() {
 	const navigate = useNavigate();
   return (
     <>
-      <HeroCTA src={"https://placehold.co/600x800"}>
+      <HeroCTA src={Mockup}>
 				<Button onClick={() => navigate("/calculator")}>Use nossa calculadora</Button>
 				<Button onClick={() => navigate("/doc")} variant="outline">Leia mais</Button>
 			</HeroCTA>
