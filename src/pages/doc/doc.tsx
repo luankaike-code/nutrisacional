dbcb4d66b5f1a529cea0c22227f8f0367b565f2f
luankaike-code <luankaikemacksantos@gmail.com>
2026-09-24 22:05:27 -0300

    Add complete-icon-doc on Doc page

--- src/pages/doc/doc.tsx
+++ src/pages/doc/doc.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import ReactMarkdown from 'react-markdown';
 import remarkDirective from "remark-directive";
 import { CalloutBlock, remarkCallouts } from "./components";
+import CompleteIcon from "@/assets/complete-icon-doc.svg"
 
 function Doc() {
 	const [content, setContent] = useState('');
@@ -14,13 +15,16 @@ function Doc() {
 	}, []);
 
 	return (
-		<div style={{ padding: '20px' }}>
+		<div className="p-8">
+			<div className="h-32 mb-24 flex flex-col justify-center">
+				<img className="h-full" src={CompleteIcon} />
+			</div>
 			<ReactMarkdown
 				remarkPlugins={[remarkDirective, remarkCallouts]}
 				components={{
-					h1: Title,
-					h2: SubTitle,
-					callout: ({ variant, children }) => <CalloutBlock variant={variant}>{children}</CalloutBlock>
+					h1: (props) => <Title className="mt-4" {...props} />,
+					h2: (props) => <SubTitle className="mt-2" {...props} />,
+					callout: ({ variant, children }) => <CalloutBlock variant={variant}>{children}</CalloutBlock>,
 				}}
 			>
 				{content}
